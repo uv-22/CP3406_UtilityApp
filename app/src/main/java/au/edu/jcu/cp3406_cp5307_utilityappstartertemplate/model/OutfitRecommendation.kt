4bd5205281df4e11ad17a52ccdf72581da576347
uv-22 <ydave045@gmail.com>
2026-06-23 17:10:28 +0800

    Create outfit recommendation model

---
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/model/OutfitRecommendation.kt
@@ -0,0 +1,8 @@
+package au.edu.jcu.cp3406_cp5307_utilityappstartertemplate.model
+
+data class OutfitRecommendation(
+    val title: String,
+    val clothingAdvice: String,
+    val weatherAdvice: String,
+    val accessoryAdvice: String
+)
